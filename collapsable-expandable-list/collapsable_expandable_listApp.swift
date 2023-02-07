74df227ed61e4e58d349da06543a1648b380c3fc
Ondrej Kvasnovsky <ondrej.kvasnovsky@gmail.com>
2023-02-07 09:38:34 -0800

    Initial Commit

---
+++ collapsable-expandable-list/collapsable_expandable_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  collapsable_expandable_listApp.swift
+//  collapsable-expandable-list
+//
+//  Created by Ondrej Kvasnovsky on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct collapsable_expandable_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
